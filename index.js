ce46d86fb72cdf3f8a72fe451c66071b4cbdb5b8
Divyanshu Rajput <divyanshurajput709@gmail.com>
2025-01-01 16:11:46 +0530

    website completed

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
-const express = require('express');
+require('dotenv').config()
+console.log(process.env.MONGO_URL)
+const express = require("express");
+const mongoose = require("mongoose");
+
+const { userRouter } = require("./routes/user");
+const { courseRouter } = require("./routes/course");
+const { adminRouter } = require("./routes/admin");
 const app = express();
+app.use(express.json());
 
 
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/admin", adminRouter);
+app.use("/api/v1/course", courseRouter);
 
-app.post('/user/signup', (req, res) => {
+async function main() {
+    await mongoose.connect(process.env.MONGO_URL)
+    app.listen(3000);
+    console.log("listening on port 3000")
+}
 
-});
-
-app.post('/user/signin', (req, res) => {
-
-});
-
-app.get('/user/purchases', (req, res) => {
-
-});
-
-app.post('/course/purchase', (req, res) => {
-
-});
-
-
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
+main()
